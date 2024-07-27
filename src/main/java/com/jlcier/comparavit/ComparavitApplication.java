2433cbfb118e6f9e5ec8ab449352768874f58179
jlcier <145858443+jlcier@users.noreply.github.com>
2024-07-26 22:21:40 -0300

    first commit

---
+++ src/main/java/com/jlcier/comparavit/ComparavitApplication.java
@@ -0,0 +1,13 @@
+package com.jlcier.comparavit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparavitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparavitApplication.class, args);
+	}
+
+}
